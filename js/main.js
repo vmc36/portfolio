9188cf5a1cd191834a138093671eb910d0ff582a
Vinicius <vmcmodesto@gmail.com>
2022-04-21 12:19:39 -0300

    bkp

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
-const swiper = new Swiper('.swiper', {
 
-    autoplay:{
-      delay:1000
-    },
-
-    slidesPerView: 2,
-    effect: 'coverflow',
-    spaceBetween:1,
-    coverflowEffect: {
-    rotate: 1,
-    stretch:1,
-    slideShadows: false,
-    centeredSlides:true,
-    rewind:true,
-  },
-  });
-
+/* Main Function AOS*/
+AOS.init();
 
--- js/materializecss.js
+++
@@ -1,4 +0,0 @@
-var elem = document.querySelector('.collapsible.expandable');
-var instance = M.Collapsible.init(elem, {
-  accordion: false
-});
